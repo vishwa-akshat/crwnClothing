67d534daeb5e8dbb368513088e89ce27a4efa14e
vishwa-akshat <akvs0707@gmail.com>
2021-04-26 16:12:35 +0530

    Finished styling menu items

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './homepage.styles.scss';
+
+import Directory from '../../components/directory/directory.components';
+
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <Directory/>
+        </div>
+    )
+}
