3cb8f9183e3f28ac634605cfd5e452c7ba178940
vishwa-akshat <akvs0707@gmail.com>
2021-04-27 14:48:25 +0530

    update menu-item to use with withRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/homepage.component';
+
+const Hatspage = ()=>(
+  <div>
+    <h1>Hats PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage}/>
+        <Route exact path="/shop/hats" component={Hatspage}/>
+      </Switch>
     </div>
   );
 }
